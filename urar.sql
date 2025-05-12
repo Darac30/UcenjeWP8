1a38b3e82870be06dd05a808c955a9b4af1e48df
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-05-12 21:25:44 +0200

    Update urar.sql

--- urar.sql
+++ urar.sql
@@ -2,9 +2,9 @@
 
 --use urar;
 
-create table korisnici (
-id int,
-ime varchar (20),
-prezime varchar (30),
-kontakt int
-);
+--create table korisnici (
+--id int,
+--ime varchar (20),
+--prezime varchar (30),
+--kontakt int
+--);
