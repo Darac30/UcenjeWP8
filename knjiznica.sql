423ac3260aa6461631720c6c7c74902e1f1d4ef1
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-05-15 21:41:40 +0200

    Create knjiznica.sql

---
+++ knjiznica.sql
@@ -0,0 +1,15 @@
+use master;
+go
+drop database if exists knjiznica;
+go
+create database knjiznica;
+go
+use knjiznica;
+
+create table osobe(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+adresa varchar(50),
+email varchar(50)
+);
