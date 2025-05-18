8f75ccae7c26d5da05b73f764117e3abf2942bf0
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-05-18 22:20:00 +0200

    Create novine.sql

---
+++ novine.sql
@@ -0,0 +1,15 @@
+﻿use master;
+go
+drop database if exists novine;
+go
+create database novine collate Croatian_CI_AS;
+go
+use novine;
+
+create table autor (
+sifra int not null primary key identity (1,1),
+ime varchar(20) not null,
+prezime varchar(30) not null,
+datumrodjenja datetime
+);
+
