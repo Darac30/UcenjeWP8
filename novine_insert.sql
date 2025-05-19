366c43c4bdc29bc2e273b00452d8f14620819a9f
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-05-19 22:09:56 +0200

    Create novine_insert.sql

---
+++ novine_insert.sql
@@ -0,0 +1,25 @@
+﻿use novine;
+
+select * from autor;
+
+insert into autor (ime, prezime, datumrodjenja) 
+values ('Ivo', 'Andric', '1933-04-15');
+
+select * from izdavac;
+
+insert into izdavac (naziv, aktivan)
+values ('Hermann', 1);
+
+select * from mjesto;
+
+insert into mjesto (naziv, drzava) values 
+('Osijek', 'Hrvatska'),
+('Ljubljana', 'Slovenija'),
+('Skoplje', 'Makedonija');
+
+update mjesto set 
+naziv='Kranj' where sifra=2;
+
+
+
+
