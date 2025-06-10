73fd30373ce194d826fa83769560ffac6a7893fa
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-06-10 18:56:49 +0200

    UlazIzlaz

---
+++ BACKEEND/Ucenja/E01UlazIzlaz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenja
+{
+    internal class E01UlazIzlaz
+    {
+
+        // unutar klase radimo metodu
+        public static void Izvedi() 
+        {
+
+
+            Console.Write("Pozdrav svijetu");
+            Console.WriteLine("Isti red");
+            Console.WriteLine("Osijek je \n super");
+            Console.WriteLine("\t\t ovo je uvuceno");
+            Console.Write("Unesi svoje ime: ");
+
+            // varijabla ime
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unijeli ste {0}", ime);
+        
+
+
+        } // kraj metode
+
+
+
+    }   // kraj klase
+}   // kraj imenskog prostora (namespace)
--- BACKEEND/Ucenja/Program.cs
+++ BACKEEND/Ucenja/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Ucenja;
+
+E01UlazIzlaz.Izvedi();
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
