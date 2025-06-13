1a1a96cab606f29dabef1e594840507180691563
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-06-13 20:53:27 +0200

    Varijable

---
+++ BACKEEND/Ucenja/E02Varijable.cs
@@ -0,0 +1,73 @@
+﻿namespace Ucenja
+{
+    internal class E02Varijable
+    {
+        public static void Izvedi() 
+        {
+            // ovo je unutar metode
+
+            Console.Write("Unesi svoje ime: ");
+
+            // deklaracija varijable
+            // tip podatka i ime
+
+            string ime;
+
+            // dodijeljivanje vrijednosti varijabli
+            ime = Console.ReadLine();
+
+            // ispisivanje sadržaja varijable
+            Console.WriteLine(ime);
+
+            Console.WriteLine(ime[0]); // 1. znak unesenog imena
+            // string je niz znakova i prvi znak u tom nizu se nalazi na indeksu 0
+
+
+            // deklariranje i dodjeljivanje u istoj liniji
+            char znak = '@';   // može sadržavati samo 1 znak i u pravilu ga nećemo koristiti
+
+            Console.WriteLine(znak);
+
+            Console.WriteLine((int)znak);  // (int) je cast; znakovi su brojevi prema ASCII tablici
+
+            int cijeliBroj = int.MaxValue;
+
+            Console.WriteLine(int.MaxValue);
+
+            Console.WriteLine(cijeliBroj+1);
+
+            Console.Write("Unesi broj cipela: ");
+
+            int brojCipela = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(brojCipela+1);
+
+            bool logickiTip = true; // druga vrijednost je false
+
+            float decimalniBroj = 3.14f;
+
+            double velikiDecimalniBroj = 3.14;
+
+            decimal db = 3.14M;
+
+
+
+
+
+
+
+
+
+            
+
+
+
+        
+        
+        
+        
+        }
+
+
+    }
+}
---
+++ BACKEEND/Ucenja/E02Z1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenja
+{
+    internal class E02Z1
+    {
+        public static void Izvedi()
+
+        {
+            Console.WriteLine("E01Z1");
+
+            Console.Write("Unesi iznos plaće (npr.522,34): ");
+
+            double placa = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(placa);
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
