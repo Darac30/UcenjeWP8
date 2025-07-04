bba349de4e556b44f51944534974b060fd5228ca
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-07-04 17:36:49 +0200

    Update Program.cs

--- BACKEEND/Ucenja/Program.cs
+++ BACKEEND/Ucenja/Program.cs
@@ -11,7 +11,26 @@ using Ucenja;
 
 //E04UvjetnoGrananjeIf.Izvedi();
 
-E05UvjetnoGrananjeSwitch.Izvedi();
+//E05UvjetnoGrananjeSwitch.Izvedi();
+
+//E06Nizovi.Izvedi();
+
+//E06Zad1.Izvedi();
+
+//E07ForPetlja.Izvedi();
+
+//E08WhilePetlja.Izvedi();
+
+//E09DoWhile.Izvedi();
+//E10ForEach.Izvedi();
+//E11TryCatch.Izvedi();
+//E11Z01.Izvedi();
+//E12Metode.Izvedi();
+//E13Rekurzija.Izvedi();
+//E11Z02.Izvedi();
+
+// ključna riječ "new" poziva konstruktor klase
+new Ucenja.E14KlasaObjekt.Program();
 
 
 
