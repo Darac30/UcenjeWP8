7dc2e9d449029c43a75b4210d8139b5c9983aef4
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-07-04 17:36:27 +0200

    Create E11Z02.cs

---
+++ BACKEEND/Ucenja/E11Z02.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Ucenja
+{
+    internal class E11Z02
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("E11Z02");
+            // Zadatak: program unosi ime osobe i ispisuje uneseno ime
+
+            string ime = Pomocno.UcitajString("Unesi ime osobe");
+            if(!Pomocno.isCijeliBroj(ime))
+            {
+                Console.WriteLine(ime);
+            }
+            
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
