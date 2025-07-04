55e7bb5dbdf42a611b4830b017e0b2c22a6e54ef
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-07-04 17:36:35 +0200

    Create E13Rekurzija.cs

---
+++ BACKEEND/Ucenja/E13Rekurzija.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenja
+{
+    internal class E13Rekurzija
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine(zbroj(100));
+        }
+
+        private static int zbroj(int i)
+        {
+
+            // uvjet prekida rekurzije
+            if(i==1)
+            {
+                return 1;
+            }
+
+            return i + zbroj(i - 1);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
