b97c689107548518f3657c11dd1e294a476058d4
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-07-05 16:22:05 +0200

    Create Mjesto.cs

---
+++ BACKEEND/Ucenja/E14KlasaObjekt/Mjesto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ucenja.E14KlasaObjekt
+{
+    // POCO - Plain Old C# Object (klase koje imaju samo svojstva nazivamo POCO)
+    public class Mjesto
+    {
+        public string? PostanskiBroj { get; set; }
+        public string? Naziv { get; set; }
+
+        public Zupanija? Zupanija { get; set; }
+    }
+}
