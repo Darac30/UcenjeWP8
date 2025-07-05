e4e67fc5ca990416074e1e9b28ccb42ec0fd4f73
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-07-05 16:22:00 +0200

    Create Automobil.cs

---
+++ BACKEEND/Ucenja/E14KlasaObjekt/Automobil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenja.E14KlasaObjekt
+{
+    public class Automobil
+    {
+        public int Sifra { get; set; }
+        public string Marka { get; set; } = ""; // ovo ="" znači da ne može biti null
+
+        public bool  Automatik { get; set; }
+
+        public DateTime? DatumProizvodnje { get; set; }
+
+        public decimal Cijena { get; set; }
+
+
+    }
+}
