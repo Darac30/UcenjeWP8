616ec9bc7f116607522cc8ec6f983e81a2f3c060
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-07-05 16:22:19 +0200

    Create Zupanija.cs

---
+++ BACKEEND/Ucenja/E14KlasaObjekt/Zupanija.cs
@@ -0,0 +1,9 @@
+﻿namespace Ucenja.E14KlasaObjekt
+{
+    public class Zupanija
+    {
+        public string Naziv { get; set; } = "";
+
+        public Osoba Zupan { get; set; } = new Osoba();
+    }
+}
