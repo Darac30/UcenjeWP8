090ef5d03775a7d1ee3e88bb5eb6d75b17022a19
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-07-14 17:14:41 +0200

    Create Predavac.cs

---
+++ BACKEEND/Ucenja/E15Nasljeđivanje/Predavac.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenja.E15Nasljeđivanje
+{
+    // u C# jedna klasa može naslijediti samo jednu nadklasu
+    public class Predavac : Osoba
+    {
+        public string IBAN { get; set; } = "";
+    }
+}
