9f33de8481238643ca4e06312cd3bd8799547562
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-07-14 17:14:32 +0200

    Create Osoba.cs

---
+++ BACKEEND/Ucenja/E15Nasljeđivanje/Osoba.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Ucenja.E15Nasljeđivanje
+{
+    public class Osoba : Object     // sve klase nasljeđuju klasu Object htjele to one ili ne
+    {
+        public int Sifra { get; set; }
+
+        public string Ime { get; set; } = "";
+
+        public string Prezime { get; set; } = "";
+
+        public string? Email { get; set; }
+
+
+        public override string ToString()
+        {
+            return Ime + " " + Prezime;
+        }
+    }
+}
