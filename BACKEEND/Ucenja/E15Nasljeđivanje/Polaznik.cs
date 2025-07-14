1edb64e915de19dd5ae1e42798816de74fc3e713
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-07-14 17:14:36 +0200

    Create Polaznik.cs

---
+++ BACKEEND/Ucenja/E15Nasljeđivanje/Polaznik.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Ucenja.E15Nasljeđivanje
+{
+    // klasa Polaznik naslijeđuje sva javna i zaštićena svojstva klase Osoba te svih drugih klasa koje osoba naslijeđuje
+    public class Polaznik : Osoba  
+    {
+        public string Telefon { get; set; } = "";
+
+        // prepisivanje metoda (method override)
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + Telefon;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            var p = obj as Polaznik;
+            if (p.Ime.Equals(Ime) &&
+                p.Prezime.Equals(Prezime) && 
+                p.Telefon.Equals(Telefon)) return true;
+
+            return false;
+            {
+                
+            }
+        }
+
+
+    }
+}
