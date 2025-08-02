9b04d2178d7555f62b439a27c629eb0685fcef27
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-02 21:16:07 +0200

    Create PotencijalniPolaznik.cs

---
+++ BACKEEND/Ucenja/E15Nasljeđivanje/PotencijalniPolaznik.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenja.E15Nasljeđivanje
+{
+    // sve iz Osobe mi odgovara, a od Osobe ne mogu napraviti klasu jer je Osoba apstraktna
+    public class PotencijalniPolaznik : Osoba
+    {
+    }
+}
