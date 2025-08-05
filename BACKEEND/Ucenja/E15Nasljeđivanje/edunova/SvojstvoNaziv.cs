a3592bae039dc497e40597d55994b0baf571f569
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-05 19:45:29 +0200

    Update SvojstvoNaziv.cs

--- BACKEEND/Ucenja/E15Nasljeđivanje/edunova/SvojstvoNaziv.cs
+++ BACKEEND/Ucenja/E15Nasljeđivanje/edunova/SvojstvoNaziv.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Ucenja.E15Nasljeđivanje.edunova
 {
-    internal class SvojstvoNaziv
+    public abstract class SvojstvoNaziv : Entitet
     {
+        public string Naziv { get; set; } = "";
     }
 }
