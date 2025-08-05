15db1cf3a8f45cc88929e8c5c59653ece3e988cf
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-05 19:45:39 +0200

    Create Polaznik.cs

---
+++ BACKEEND/Ucenja/E16Polimorfizam/Polaznik.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace Ucenja.E16Polimorfizam
+{
+    public class Polaznik : Osoba
+    {
+        public string? Telefon { get; set; }
+
+        public override void Greska(string poruka)
+        {
+            // todo: Zavrsi
+            //pojam tehnički dug: technical debt
+        }
+
+        public override string Pozdravi()
+        {
+            return new StringBuilder().Append("Dobar dan ").Append(Ime).Append(", polazniče ").Append(Telefon).ToString();
+        }
+    }
+}
