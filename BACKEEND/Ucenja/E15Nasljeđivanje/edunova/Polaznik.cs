63e8a4ef63c2031a8dce1beaf16d1a8a618e51a7
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-05 17:17:18 +0200

    Create Polaznik.cs

---
+++ BACKEEND/Ucenja/E15Nasljeđivanje/edunova/Polaznik.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Ucenja.E15Nasljeđivanje.edunova
+{
+    public class Polaznik : Entitet
+    {
+
+        public string Ime { get; set; } = "";
+
+        public string Prezime { get; set; } = "";
+
+        public string? Email { get; set; }
+    }
+}
