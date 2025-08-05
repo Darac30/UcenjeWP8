c4a7b281c680a39828c3ef83167ee17c91ee38d4
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-05 19:45:48 +0200

    Create Ravnatelj.cs

---
+++ BACKEEND/Ucenja/E16Polimorfizam/Ravnatelj.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Ucenja.E16Polimorfizam
+{
+    public class Ravnatelj : Osoba
+    {
+        public override void Greska(string poruka)
+        {
+            
+        }
+
+        public override void Log(string poruka) 
+        {
+            Console.WriteLine(poruka);
+        }
+
+        public override string Pozdravi()
+        {
+            return "";
+        }
+    }
+}
