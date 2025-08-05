5cf23fa053959dd64a9cae6f572f4d15a2571297
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-05 17:17:21 +0200

    Create Grupa.cs

---
+++ BACKEEND/Ucenja/E15Nasljeđivanje/edunova/Grupa.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Ucenja.E15Nasljeđivanje.edunova
+{
+    public class Grupa : SvojstvoNaziv
+    {
+
+        public Smjer Smjer { get; set; } = new Smjer();   // ide ovako zato što je vanjski ključ
+
+        public string? Predavac { get; set; }
+
+        public Polaznik[]? Polaznici { get; set; }    // lista polaznika koja može biti null
+
+
+    }
+
+    // tablicu "članovi" ovdje nismo nigdje unijeli jer se ona sastoji od samo dva vanjska ključa;
+    // kada bismo u toj tablici imali još nekih atributa osim navedenih, tada bismo ju unijeli.
+}
