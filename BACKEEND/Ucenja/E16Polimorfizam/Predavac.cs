01cfdbd6ccb4c2e53ad472acd569db483438ba9d
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-05 19:45:42 +0200

    Create Predavac.cs

---
+++ BACKEEND/Ucenja/E16Polimorfizam/Predavac.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Ucenja.E16Polimorfizam
+{
+    public class Predavac : Osoba
+    {
+        public string IBAN { get; set; }
+
+        public override void Greska(string poruka)
+        {
+            
+        }
+
+        public override string Pozdravi()
+        {
+            return Ime + " " + IBAN;
+        }
+    }
+}
