c999e0b878e57c25c27d42cc2faf8cee37433271
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-08 20:51:09 +0200

    Create Zadatak1.cs

---
+++ BACKEEND/Ucenja/E17Vjezbanje/Zadatak1.cs
@@ -0,0 +1,19 @@
+﻿namespace Ucenja.E17Vjezbanje
+{
+    public class Zadatak1
+    {
+
+        private int brojIzvodenja = 0;
+        
+        public void Izvedi()
+        {
+            brojIzvodenja++;
+            int pb = Pomocno.ucitajCijeliBroj("Unesi prvi broj");
+            int db = Pomocno.ucitajCijeliBroj("Unesi drugi broj");
+            Console.WriteLine("{0} + {1} = {2}", pb, db, pb + db);
+            Console.WriteLine("Završio {0}. izvođenje", brojIzvodenja);
+
+        }
+         
+    }
+}
