1ca3ff7cac10264bd14d674f16e2e4a4a39a2e7e
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-08 20:50:53 +0200

    Create Obrada.cs

---
+++ BACKEEND/Ucenja/E18GSALE/Obrada.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenja.E18GSALE
+{
+    public class Obrada<T> // T je oznaka TYPE, a može biti bilo koje slovo ili niz slova
+    {
+        public T? PredmetObrade { get; set; }
+
+        [Obsolete("Ovo više nemoj koristiti")]  // deprecated; označava da smo to prestali korsititi, ali ga nećemo izbaciti jer kod se možda još koristi
+        public void Posao()
+        {
+            Console.WriteLine("Obrađujem {0}",PredmetObrade);
+
+            // Primjer depricated u C#
+            // Thread.ResetAbort();
+        }
+    }
+}
