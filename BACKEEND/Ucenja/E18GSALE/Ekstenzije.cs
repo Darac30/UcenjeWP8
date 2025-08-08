27ec833fb6a68be88dc7a7640905cbd5fc445cd2
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-08 20:51:03 +0200

    Create Ekstenzije.cs

---
+++ BACKEEND/Ucenja/E18GSALE/Ekstenzije.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Ucenja.E18GSALE
+
+    // Ekstenzije su metode koje možemo nakačiti na bilo koji tip podatka za koji je ekstenzija napisana; ako je napisana za Object može ići na bilo koji tip podatka
+{
+    public static class Ekstenzije
+    {
+
+        public static void EdunovaProvjera(this string s)
+        {
+            Console.WriteLine(s.Substring(2,4));
+
+
+        }
+
+        public static int Broj(this IObrada obrada)  // Broj se može nakačiti na instancu bilo koje klase koja je implementirala IObradu
+
+        {
+            string s = obrada.Metoda();
+            return s.Length;
+        }
+    }
+}
