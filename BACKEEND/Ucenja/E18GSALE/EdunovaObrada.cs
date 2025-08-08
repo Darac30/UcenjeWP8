ddae7e6b98b45579a3d3f68fddeef8e364c33291
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-08 20:51:06 +0200

    Create EdunovaObrada.cs

---
+++ BACKEEND/Ucenja/E18GSALE/EdunovaObrada.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenja.E18GSALE
+{
+    public class EdunovaObrada<T> : IObrada where T : Entitet  // mora biti bilo koja klasa koja je naslijedila Entitet
+    {
+        public T? Entitet { get; set; }
+
+        public string Metoda()
+        {
+            return "Edunova";
+        }
+
+        public void OdradiPosao()
+        {
+            Posao();
+        }
+
+        [Obsolete("Više nemoj koristiti")]
+        public void Posao()
+        {
+            Console.WriteLine(Entitet?.Sifra);
+        }
+
+    }
+}
