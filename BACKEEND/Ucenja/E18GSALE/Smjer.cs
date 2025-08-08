eab758cc4504b4c43b6c0376c5c3935e1df1e59a
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-08 20:50:45 +0200

    Create Smjer.cs

---
+++ BACKEEND/Ucenja/E18GSALE/Smjer.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Ucenja.E18GSALE
+{
+    // sučelja mogu biti generička IComparable<Smjer>
+    public class Smjer : Entitet, IComparable<Smjer>, IObrada  // Smjer naslijeđuje Entitet, ali implementira IComparable (sučelje za uspoređivanje)
+    {
+        public string Naziv { get; set; } = "";
+
+        public int CompareTo(Smjer? other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            return Naziv.CompareTo(other.Naziv);
+        }
+
+        public string Metoda()
+        {
+            return "Ana";
+        }
+
+        public void OdradiPosao()
+        {
+            Console.WriteLine("Smjer");
+        }
+
+        public override string ToString()
+        {
+            return Naziv ?? "";  // Ako je naziv null, vrati prazno ""
+        }
+    }
+}
