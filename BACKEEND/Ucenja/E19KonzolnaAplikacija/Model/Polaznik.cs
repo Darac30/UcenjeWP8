3a54cc74a729e5841e32a563af5b9d4af1b9bc3f
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-11 16:06:33 +0200

    Create Polaznik.cs

---
+++ BACKEEND/Ucenja/E19KonzolnaAplikacija/Model/Polaznik.cs
@@ -0,0 +1,15 @@
+﻿namespace Ucenje.E19KonzolnaAplikacija.Model
+{
+    public class Polaznik:Entitet, IComparable<Polaznik>    // IComparable je sučelje (interface), a <Polaznik> je generic(generik)
+    {
+        public string Ime { get; set; } = "";
+        public string Prezime { get; set; } = "";
+        public string? OIB { get; set; }
+        public string? Email { get; set; }
+
+        public int CompareTo(Polaznik? other)
+        {
+            return Prezime.CompareTo(other.Prezime);
+        }
+    }
+}
